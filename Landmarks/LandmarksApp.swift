fe296807a6347baa3b064c9fb8f0149e7ed9971b
Vid Čufar <vid.cufar@gmail.com>
2021-09-16 23:02:10 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vid Čufar on 16/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
