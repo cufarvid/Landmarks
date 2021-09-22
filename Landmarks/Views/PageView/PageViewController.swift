69133855891832c82ee43e0b6e29f272c4199058
Vid Čufar <vid.cufar@gmail.com>
2021-09-22 22:36:04 +0200

    Add page control to categories home view

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -21,19 +22,20 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             navigationOrientation: .horizontal
         )
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]],
+            [context.coordinator.controllers[currentPage]],
             direction: .forward,
             animated: true
         )
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -72,6 +74,19 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             
             return controllers[index + 1]
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool)
+        {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
     }
 }
